cf98856c7258d793548e4ea0a77c04606845dd37
Alex <ashangin@hotmail.com>
2021-02-03 12:48:45 +0300

    Created Login and Register Views

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
+
 class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "loggedIn")
+        
+        if !isLoggedIn {
+            let vc = LogInViewController()
+            
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            
+            present(nav, animated: false)
+        }
+    }
 
 }
 
