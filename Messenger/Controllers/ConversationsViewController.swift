7439b54c34ceb929ae4992e119bc813c5caf573d
Alex <ashangin@hotmail.com>
2021-02-07 13:03:05 +0300

    Bring in pods

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -5,10 +5,14 @@ import FirebaseAuth
 
 class ConversationsViewController: UIViewController {
 
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         
     }
 
